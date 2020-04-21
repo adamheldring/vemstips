58278bb89ce1e2dbe65a3cda480fd53e45b70833
A-Game <javascript@adamheldring.com>
2020-04-21 15:42:30 +0200

    refactor getWeekNumber function to ES6

--- getWeekNumber.js
+++ getWeekNumber.js
@@ -1,13 +1,13 @@
-function getWeekNumber(d) {
+function getWeekNumber(currentDate) {
     // Copy date so don't modify original
-    d = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+    let d = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
     // Set to nearest Thursday: current date + 4 - current day number
     // Make Sunday's day number 7
     d.setDate(d.getDate() + 4 - (d.getDay()||7));
     // Get first day of year
-    var yearStart = new Date(d.getFullYear(),0,1);
+    const yearStart = new Date(d.getFullYear(),0,1);
     // Calculate full weeks to nearest Thursday
-    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    const weekNo = Math.ceil((((d - yearStart) / 86400000) + 1)/7);
 
     // Return object with year and week number
     return {
